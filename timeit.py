58ef47ea23f5e30d59255b6371454d20eb7e9990
mkao823 <99455489+mkao823@users.noreply.github.com>
2022-03-09 23:22:20 -0800

    Update timeit.py

--- timeit.py
+++ timeit.py
@@ -1,15 +1,17 @@
 import time
 def calculate_time(func):
     def wrapper():
-        begin = time.time()
+        timeOne = time.time()
         func()
-        term = time.time()
-        delay = term - begin
+        timeTwo = time.time()
+        delay = timeTwo - timeOne
         print("Total Time %s" % delay)
     return wrapper
 
 @calculate_time
 def run1(): 
     print("")
+
+run1()
 
 

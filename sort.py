0c11f24b65357c6ad6134ae20947d22e8fe0eb79
mkao823 <99455489+mkao823@users.noreply.github.com>
2022-03-09 20:43:00 -0800

    Update sort.py

--- sort.py
+++ sort.py
@@ -1,4 +1,4 @@
-def sort_list(list):
+def sort_list(myList):
     '''this is summary line
 
     now we can use this to describe program
@@ -6,19 +6,19 @@ def sort_list(list):
     in sort list, n is length of input list
     
     ''' 
-    n = len(list)
+    n = len(myList)
     i = 0
     while i < n - 1:
         j = 0
         while j < n - i - 1:
-            if (list[j] > list[j+1]):
-                temp = list[j]
-                list[j] = list[j+1]
-                list[j+1] = temp
+            if (myList[j] > myList[j+1]):
+                temp = myList[j]
+                myList[j] = myList[j+1]
+                myList[j+1] = temp
             j+=1
         i+=1
 
-    return list
+    return myList
             
     
 

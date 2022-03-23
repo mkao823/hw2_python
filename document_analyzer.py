5fba72acc87704d2adf6782f91ddf56e49a85398
mkao823 <99455489+mkao823@users.noreply.github.com>
2022-03-22 18:38:41 -0700

    Update document_analyzer.py

--- document_analyzer.py
+++ document_analyzer.py
@@ -9,5 +9,15 @@ def sorter(nameOfFile):
         if word in words:
             words[word] += 1
         else:
-            words[word] = 1        
-        
+            words[word] = 1
+            
+    index = 0
+    unique = sorted(words.items(), key = lambda x:x[1], reverse = True)
+    unique = dict(unique)
+
+    print("\r")
+    for word, count in unique.items():
+        print("%s: %s" % (word, count))
+        index += 1
+        if(index == 5):
+            break
